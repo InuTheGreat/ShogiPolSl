c77c70fca830ecf842865b1c1d3e528cca62b2f8
therealmp213 <therealmp213@outlook.com>
2025-06-09 01:41:58 +0200

    patrz Google Docs, dziennik z 9.06, wybacz, ale trochę przeobraziłem to, co zastałem wcześniej, ale mam nadzieję, że nada to jakiś nowy, bardziej skoncentrowany kierunek ;p
    (plansza startowa szogi, gracz może manipulować położeniem figur, pętla nieskończona aż gracz sam od siebie zakończy)

--- main.cpp
+++ main.cpp
@@ -1,16 +1,77 @@
-#include "klasy.h"
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+const int SIZE = 9;
+
+// Inicjalizacja planszy zgodnie z podanym schematem
+int board[SIZE][SIZE] = {
+    {7, 6, 5, 4, 1, 4, 5, 6, 7},
+    {0, 2, 0, 0, 0, 0, 0, 3, 0},
+    {8, 8, 8, 8, 8, 8, 8, 8, 8},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {8, 8, 8, 8, 8, 8, 8, 8, 8},
+    {0, 3, 0, 0, 0, 0, 0, 2, 0},
+    {7, 6, 5, 4, 1, 4, 5, 6, 7}
+};
+
+void printBoard() {
+    cout << "\n  ";
+    for (int x = 0; x < SIZE; x++) {
+        cout << setw(2) << x+1 << " ";
+    }
+    cout << "\n";
+
+    for (int y = 0; y < SIZE; y++) {
+        cout << y+1 << " ";
+        for (int x = 0; x < SIZE; x++) {
+            cout << setw(2) << board[y][x] << " ";
+        }
+        cout << "\n";
+    }
+}
+
+bool isValidPosition(int x, int y) {
+    return x >= 0 && x < SIZE && y >= 0 && y < SIZE;
+}
 
 int main() {
+    bool running = true;
+    int fromX, fromY, toX, toY;
+    char input;
 
-    plansza p1;
+    while(running) {
+        printBoard();
 
-    figura pionek1("Pionek1",1,6,4);
-    p1.wyswietlPlansze();
-    pionek1.ruchFigury(p1);
-    p1.wyswietlPlansze();
+        cout << "\nWybierz figure (x y) lub 'q' aby wyjsc: ";
+        cin >> input;
+        if(input == 'q') break;
+        cin.putback(input);
 
+        cin >> fromX >> fromY;
+        fromX--; fromY--; // Konwersja na indeksy od 0
 
+        if(!isValidPosition(fromX, fromY) || board[fromY][fromX] == 0) {
+            cout << "Nieprawidlowa pozycja startowa!\n";
+            continue;
+        }
+
+        cout << "Podaj docelowa pozycje (x y): ";
+        cin >> toX >> toY;
+        toX--; toY--;
+
+        if(!isValidPosition(toX, toY)) {
+            cout << "Nieprawidlowa pozycja docelowa!\n";
+            continue;
+        }
+
+        // Wykonanie ruchu
+        board[toY][toX] = board[fromY][fromX];
+        board[fromY][fromX] = 0;
+    }
 
     return 0;
-
 }
